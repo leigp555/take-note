08576ba3c4c12b93ef3c78bd923d6d99746578d5
lgp <907090585@qq.com>
2022-06-06 09:29:25 +0000

    publish 19f29c22f8f320d5d688a17da2644a94e1f73f56

---
+++ js/contains-eee41f85.js
@@ -0,0 +1,1 @@
+import{s as e}from"./Portal-7e6477da.js";function t(t,n,r,o){if(t&&t.addEventListener){var s=o;void 0!==s||!e||"touchstart"!==n&&"touchmove"!==n&&"wheel"!==n||(s={passive:!1}),t.addEventListener(n,r,s)}return{remove:function(){t&&t.removeEventListener&&t.removeEventListener(n,r)}}}function n(e,t){return!!e&&e.contains(t)}export{t as a,n as c};
