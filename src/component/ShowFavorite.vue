e4bb751112e6fb0c9cbacf6fee5dd907ff15ac03
leigp <907090585@qq.com>
2022-06-08 17:33:52 +0800

    style: 改造我喜欢组件

--- src/component/ShowFavorite.vue
+++ src/component/ShowFavorite.vue
@@ -1,43 +1,6 @@
-<script lang="ts" setup>
-import ArticlePage from '@/component/ArticlePage.vue';
-</script>
-
 <template>
-  <div class="wrap">
-    <section class="favorite">
-      <ArticlePage />
-    </section>
-  </div>
+  <SearchAndShow />
 </template>
-
-<style lang="scss" scoped>
-.wrap {
-  display: flex;
-  height: 100%;
-  position: relative;
-  .favorite {
-    position: absolute;
-    max-height: 100%;
-    top: 0;
-    left: 0;
-    flex-grow: 1;
-    width: 100%;
-    padding: 10vh 20vw;
-    overflow-y: scroll;
-    overflow-x: hidden;
-    &::-webkit-scrollbar {
-      width: 8px;
-      height: 8px;
-    }
-    &::-webkit-scrollbar-thumb {
-      border-radius: 3px;
-      -moz-border-radius: 3px;
-      -webkit-border-radius: 3px;
-      background-color: #c3c3c3;
-    }
-    &::-webkit-scrollbar-track {
-      background-color: transparent;
-    }
-  }
-}
-</style>
+<script lang="ts" setup>
+import SearchAndShow from '@/component/SearchAndShow.vue';
+</script>
